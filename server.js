83a1f78d8d96d7853d83d5ce5707328b9c5d5772
Lakshya <harshitlucky2004@gmail.com>
2025-01-06 13:03:09 +0530

    error fixed

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
-const connectDB= require('./src/backend/db'); // Import the dbConnect function
+const {connectDB}= require('./src/backend/db'); // Import the dbConnect function
 
 // Initialize Next.js
 const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
